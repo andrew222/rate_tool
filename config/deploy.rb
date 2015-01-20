28198c0958ade63cbc2e026004e241d9d3174262
Administrator <admin@local.host>
2015-01-20 23:10:49 +0800

    MISC - deploy script bug

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:andrew222/rate_tool.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/var/www/fetch(:application)'
+set :deploy_to, "/var/www/rate_tool"
 
 # Default value for :scm is :git
 # set :scm, :git
