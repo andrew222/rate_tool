6d6a18fd43c5632673bc053145bcc67b9baafb2f
andrew <andrew@ekohe.com>
2015-01-28 16:22:25 +0800

    MISC - Enable the rangebuttons

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -1,6 +1,9 @@
 window.display_rate_chart = function(rates) {
   var dates = rates[0],
       rate_arr = rates[1];
+  $.each(rates, function(index, value){
+    value[0] = new Date(value[0]).getTime();
+  })
   var chart = $(".hight-chart .rate").highcharts("StockChart", {
     title: {
       text: "浦发银行 外汇牌价(美元)",
