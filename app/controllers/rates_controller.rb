ae8b6e68e53b9c53da9fb2ca2feeb2403c6bdb04
andrew <andrew@andrew-laptop.(none)>
2015-01-31 16:37:44 +0800

    BUG - fix tooltip date bug

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -5,7 +5,7 @@ class RatesController < ApplicationController
   # GET /rates.json
   def index
     @rates = Rate.paginate(:page => params[:page]).order("created_at desc")
-    @rate_date_arr = Rate.weekly_rate
+    @rate_date_arr = Rate.all_rates
   end
 
   # GET /rates/1
--- app/models/rate.rb
+++ app/models/rate.rb
@@ -5,7 +5,14 @@ class Rate < ActiveRecord::Base
     seven_days_ago = now - 7.days
     rates = Rate.where(created_at: (seven_days_ago..now))
     rates.collect do |rate|
-      [(rate.created_at.gmtime + 28800).strftime("%Y-%-m-%-d %H:%M"), rate.bid_fx.to_f]
+      [(rate.created_at).strftime("%Y-%-m-%-d %H:%M"), rate.bid_fx.to_f]
+    end
+  end
+
+  def self.all_rates
+    rates = Rate.all
+    rates.collect do |rate|
+      [(rate.created_at).strftime("%Y-%-m-%-d %H:%M"), rate.bid_fx.to_f]
     end
   end
 end
