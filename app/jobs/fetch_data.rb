00bc31fb9324aaa85f50700b8e8df33d91ad7cce
andrew <andrew@ekohe.com>
2015-02-03 19:09:12 +0800

    MISC - Add more banks

--- app/jobs/fetch_data.rb
+++ app/jobs/fetch_data.rb
@@ -10,7 +10,20 @@ class FetchData
     doc = Nokogiri::HTML(open("http://ebank.spdb.com.cn/net/QueryExchangeRate.do"))
     trs = doc.xpath("//table[@class='table_comm']/tr")
     us_dollar_tr = trs[4]
-    us_dollar = us_dollar_tr.xpath("./td")[2].text()
+    spdb_us_dollar = us_dollar_tr.xpath("./td")[2].text()
+    build_rate(spdb_us_dollar)
+
+    ccb_doc = Nokogiri::XML(open("http://forex.ccb.com/cn/home/news/jshckpj.xml"))
+    ccb_dollar = ccb_doc.xpath(".//ReferencePriceSettlement/CM_CURR_COD[text()='14']")
+    unless ccb_dollar.nil?
+      unless ccb_us_dollar_elem = ccb_dollar.first().parent
+        cbb_us_dollar = ccb_us_dollar_elem.xpath("./FXR_XCH_BUYIN").text()
+        build_rate(cbb_us_dollar.to_f*100.round(2).to_s)
+      end
+    end
+  end
+
+  def build_rate(us_dollar)
     unless us_dollar.match(/^\d+\.\d{2}$/).nil?
       if lastest_rate = Rate.last
 	if lastest_rate.bid_fx.to_s != us_dollar
