cc7e5c2b726f4219316e40c1b1d031626dffc255
Andrew Yang <zzcv20051122@gmail.com>
2015-02-05 21:54:44 +0800

    MISC - Chart messed up

--- app/models/spdb_rate.rb
+++ app/models/spdb_rate.rb
@@ -1,2 +1,8 @@
 class SpdbRate < Rate
+  def self.all_rates
+    rates = SpdbRate.all.order("created_at desc")
+    rates.collect do |rate|
+      [(rate.created_at).to_i*1000, rate.bid_fx.to_f]
+    end
+  end
 end
