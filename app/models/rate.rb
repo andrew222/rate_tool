8528b2d234b6ef5fc3611ba1239f2dedf0119ba4
Andrew Yang <zzcv20051122@gmail.com>
2015-02-05 22:24:54 +0800

    MISC - Add daily growth rate

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -15,4 +15,13 @@ class Rate < ActiveRecord::Base
       [(rate.created_at).to_i*1000, rate.bid_fx.to_f]
     end
   end
+
+  def self.daily_growth_rate
+    Time.zone = "Beijing"
+    now = Time.zone.now
+    yestday = now - 1.day
+    last_rate_of_yestday = SpdbRate.where(created_at: (yestday.beginning_of_day..yestday.end_of_day)).last
+    last_rate_of_today = SpdbRate.where(created_at: (now.beginning_of_day..now.end_of_day)).last
+    (last_rate_of_today.bid_fx.to_f - last_rate_of_yestday.bid_fx.to_f)/last_rate_of_yestday.bid_fx.to_f
+  end
 end
