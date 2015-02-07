c0bb8e383919ebd6a2a4c5c8e4b5bfecbd3f157e
Andrew Yang <zzcv20051122@gmail.com>
2015-02-07 20:16:29 +0800

    MISC - Use published at instead of created_at

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -3,16 +3,16 @@ class Rate < ActiveRecord::Base
   def self.weekly_rate
     now = Time.now.end_of_day
     seven_days_ago = now - 7.days
-    rates = SpdbRate.where(created_at: (seven_days_ago..now))
+    rates = SpdbRate.where(published_at: (seven_days_ago..now))
     rates.collect do |rate|
-      [(rate.created_at).to_i*1000, rate.bid_fx.to_f]
+      [(rate.published_at).to_i*1000, rate.bid_fx.to_f]
     end
   end
 
   def self.all_rates
     rates = SpdbRate.all
     rates.collect do |rate|
-      [(rate.created_at).to_i*1000, rate.bid_fx.to_f]
+      [(rate.published_at).to_i*1000, rate.bid_fx.to_f]
     end
   end
 
@@ -24,15 +24,15 @@ class Rate < ActiveRecord::Base
     end
     new_date = now - days_before.day
     old_date = new_date - 1.day
-    old_rate = BocRate.where(created_at: (old_date.beginning_of_day..old_date.end_of_day)).last
-    new_rate = BocRate.where(created_at: (new_date.beginning_of_day..new_date.end_of_day)).last
+    old_rate = BocRate.where(published_at: (old_date.beginning_of_day..old_date.end_of_day)).last
+    new_rate = BocRate.where(published_at: (new_date.beginning_of_day..new_date.end_of_day)).last
     unless old_rate.nil? || new_rate.nil?
       ((new_rate.bid_fx.to_f - old_rate.bid_fx.to_f)/old_rate.bid_fx.to_f)*100
     else
-      new_date = BocRate.last.created_at
+      new_date = BocRate.last.published_at
       old_date = new_date - 1.day
-      old_rate = BocRate.where(created_at: (old_date.beginning_of_day..old_date.end_of_day)).last
-      new_rate = BocRate.where(created_at: (new_date.beginning_of_day..new_date.end_of_day)).last
+      old_rate = BocRate.where(published_at: (old_date.beginning_of_day..old_date.end_of_day)).last
+      new_rate = BocRate.where(published_at: (new_date.beginning_of_day..new_date.end_of_day)).last
       if old_rate && new_rate
 	((new_rate.bid_fx.to_f - old_rate.bid_fx.to_f)/old_rate.bid_fx.to_f)*100
       else
