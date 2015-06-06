99605c49c4ec20966a6a873d93e6fefaf099f980
andrew222 <zzcv20051122@126.com>
2015-06-06 08:40:20 +0800

    MISC - Remove location detector first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :set_locale
+  #before_filter :set_locale
   helper :all
   helper_method :current_user_session, :current_user
 
